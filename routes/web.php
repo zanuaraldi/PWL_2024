14860ae664b641e1f411d5ebf144031792e7a5ff
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-09-06 09:38:52 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return 'Nama Saya Zanuar Aldi Syahputra <br> NIM saya 2241760062';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($Id) {
+    return 'Halaman Artikel dengan ID '. $Id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
 });
